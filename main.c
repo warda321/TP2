e8cc818d2d94ecaba74c67c33fa6a12d5b8cf781
warda321 <79303043+warda321@users.noreply.github.com>
2022-11-16 21:39:22 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,103 +1,22 @@
 /**
-  ****************************************************************************
+  ******************************************************************************
   * @file    main.c
-  * @author  Guizani Warda
-  * @brief   Macros 
+  * @author GUIZANI WARADA
+  * @brief   Bit Mask 
   * @version V1.0.0
-  * @date    25-10-2022
+  * @date    16-10-2022
   *****************************************************************************/
+
 /* Includes ------------------------------------------------------------------*/
-
-/* Macros --------------------------------------------------------------------*/
-
-#define INTEL
-#define SOM
-
-#define OTHERS
-#define MUL
+#include <stm32f4xx.h>
+#include "gpio.h"
 
 
-
-/* Private function prototypes -----------------------------------------------*/
-static int sum(int x, int y);
-static int mul(int x, int y);
-/* Private functions ---------------------------------------------------------*/
-
-/**
-  * @brief  Main program
-  * @param  None
-  * @retval None
-  */
-int main(void)
+int main()
 {
-#ifdef INTEL
- #ifdef SOM
- int SumResult=sum(1,2);
-#endif
- 
-#ifdef MIN
- int MinResult=min(1,2);
-#endif
-#endif
- 
-#ifdef OTHERS
-#ifdef MUL
- int mulResult=mul(1,2);
-#endif
-
- 
-#ifdef MAX
- int maxResult=max(1,2);
-#endif
-#endif
-  while(1)
-  
-  {   
-  }
+   GPIO_Clock_Enable(GPIOD);
+   GPIO_Config (GPIOD , PIN12 , ALTERNATE , OUTPUT_OPEN_DRAIN );
+   while(1)
+   {
+   }
 }
-#ifdef INTEL
-#ifdef SOM 
-static int sum(int x, int y)
-
-{
-  
-  return (x+y);
-  
-}
-#endif
-#ifdef MIN
-#define MIN ((x)<(y))? (x) : (y) 
-static int min(int x, int y)
-
-{
-
-
-  
-  return (MIN);
-  
-}
-#endif
-#endif
-
-#ifdef OTHERS
-  #ifdef MUL
-  static int mul(int x, int y)
-
-{
-  
-  return (x*y);
-  
-}
-  #endif
- #ifdef MAX
-#define MAX ((x)>(y))? (x) : (y))
- static int max(int x, int y)
-
-{
-
-  return (MAX);
-  
-}
-  #endif
-#endif
-/**************************************END OF FILE**************************************/
